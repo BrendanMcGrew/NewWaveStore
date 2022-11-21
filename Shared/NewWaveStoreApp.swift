26d5e4350cacbf73fe886fc98a79acba5af364a5
Brendan McGrew <s016752@students.lmsd.org>
2022-11-21 07:38:04 -0500

    Initial Commit

---
+++ Shared/NewWaveStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreApp.swift
+//  Shared
+//
+//  Created by Brendan McGrew (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NewWaveStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
